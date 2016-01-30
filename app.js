aa91602ef313399c48fe147e19e23703f8255118
Zsolt Munoz <zmunoz@gmail.com>
2016-01-30 16:18:34 -0700

    Updated app.js

--- app.js
+++ app.js
@@ -8,15 +8,3 @@ var util = require('util');
 var pxp = new playbyplay(request, cheerio);
 var sched = new schedule(request);
 var player = new players(request, cheerio);
-
-/*pxp.playbyplay('20142015','02','0002', function(e) {
-  console.log(util.inspect(e, false, null));
-});*/
-
-/*sched.seasonschedule('20142015', function(e) {
-  console.log(util.inspect(e, false, null));
-})*/
-
-player.playerpage('8470621', function(e) {
-  console.log(util.inspect(e, false, null));
-})
