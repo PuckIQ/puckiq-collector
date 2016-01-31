56f8b9bc8d04a424b48c82449cb7632a91eba911
Zsolt Munoz <zmunoz@gmail.com>
2016-01-30 22:37:49 -0700

    Updated players.js

--- games/players.js
+++ games/players.js
@@ -26,16 +26,53 @@ function playersHandler(request, cheerio) {
 				var $ = cheerio.load(modhtml);
 
         var tombstone = $('#tombstone');
-        var bioInfo = $('.bioInfo', tombstone);
-        var bio = $(bioInfo).html().replace(/(\r\n|\n|\r)/gm,"")
+        //var bioInfo = $('.bioInfo', tombstone);
+        var bio = $(tombstone).html().replace(/(\r\n|\n|\r)/gm,"")
 
         var playerpageInfo = new Object();
 
         playerpageInfo['name'] = $('.sweater').parent().html().split('&')[0].trim();
 
-        $(bio).each(function(tr, td) {
-          console.log($(td).html());
-        });
+        var countline = 0;
+        var drafted = new Object();
+        $(bio).each(function(i,v) {
+          $(v).find('td').each(function() {
+            if(countline%2 == 0) {
+              switch ($(this).text().replace(':','').toLowerCase()) {
+                case 'birthdate':
+                  var jsdate = new Date($(this).next().text().split('(')[0].trim());
+                  var utctime = new moment.tz(jsdate, 'UTC');
+                  playerpageInfo['dob'] = utctime.format();
+                  break;
+                case 'height':
+                  var feet = parseInt($(this).next().text().split('\'')[0].trim())*12;
+                  var inches = parseInt($(this).next().text().split('\'')[1].trim());
+                  playerpageInfo['height'] = feet+inches;
+                  break;
+                case 'birthplace':
+                case 'shoots':
+                  playerpageInfo['birthplace'] = $(this).next().text().trim();
+                  break;
+                case 'weight':
+                  playerpageInfo['wieght'] = parseInt($(this).next().text().trim());
+                  break;
+                case 'drafted':
+                  var splitDraft = $(this).next().text().trim().split(' ');
+                  var draftteam = splitDraft[0].replace('/','');
+                  drafted['team'] = draftteam;
+                  drafted['year'] = splitDraft[1];
+                  break;
+                case 'round':
+                  var splitDraft = $(this).next().text().trim().split(' ')[0].split('(');
+                  drafted['round'] = parseInt(splitDraft[0]);
+                  drafted['overall'] = parseInt(splitDraft[1]);
+                  playerpageInfo['drafted'] = drafted;
+              }
+              console.log($(this).text().toLowerCase() + $(this).next().text());
+            }
+            countline++
+          });
+        })
 
         callback(playerpageInfo);
       } else {
